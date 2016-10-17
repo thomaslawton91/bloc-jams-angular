4b729d81581d0e191e8eb7897f57a94076e711f2
deepanchor12 <thomaslawton91@gmail.com>
2016-10-17 10:52:22 -0400

    Inject Fixture controller to collection ctrl. Add collection controller to collection ctrl.

--- app/scripts/Services/Fixtures.js
+++ app/scripts/Services/Fixtures.js
@@ -67,7 +67,14 @@
          Fixtures.getAlbum = function () {
              return albumPicasso;
          };
+         Fixtures.getCollection = function(numberOfAlbums) {
+             var albums = [];
+             for (var i = 0; i < numberOfAlbums; i++) {
+                 albums.push(angular.copy(albumPicasso));
+             };
+             return albums;
+         };
          return Fixtures;
-     }
+     };
      angular.module('blocJams').factory('Fixtures', Fixtures);
  })();
--- dist/scripts/Controllers/AlbumCtrl.js
+++ dist/scripts/Controllers/AlbumCtrl.js
@@ -2,10 +2,5 @@
     function AlbumCtrl(Fixtures) {
         this.albumData = Fixtures.getAlbum();
         }
-<<<<<<< HEAD
-    }
     angular.module('blocJams').controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
-=======
-    angular.module('blocJams').controller('AlbumCtrl', AlbumCtrl);
->>>>>>> 73c365bd9e0f248ba9168c6f9568c97455fb653a
 })();
